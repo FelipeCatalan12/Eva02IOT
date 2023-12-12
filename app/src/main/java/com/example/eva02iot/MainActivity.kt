569a28447ac7fa6cc5d1cb59d0b3f6070dcf3a20
FelipeCatalan12 <felipeign767@gmail.com>
2023-12-11 23:16:12 -0300

    Cambios Login

--- app/src/main/java/com/example/eva02iot/MainActivity.kt
+++ app/src/main/java/com/example/eva02iot/MainActivity.kt
@@ -9,53 +9,41 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.example.eva02iot.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    //Firebase Authentication
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //Cambio a segunda vista
 
-        binding.btnSignIn.setOnClickListener{
-            try{
-                val nombre: String = binding.Username.text.toString()
-                val clave: String = binding.Clave.text.toString()
-                val repetirclave : String = binding.RepetirClave.text.toString()
+        //Inicializar Firebase con auth
 
-                if (nombre.isNotEmpty() && clave.isNotEmpty() && repetirclave.isNotEmpty() ){
-                    val intent = Intent(this, VistaBienvenida::class.java)
-                    intent.putExtra("nombre",nombre)
-                    startActivity(intent)
-                    Toast.makeText(this, "Bienvenido $nombre",Toast.LENGTH_SHORT).show()
-                }else{
-                    Toast.makeText(this, "Faltan campos por llenar",Toast.LENGTH_LONG).show()
+        val auth = Firebase.auth
+
+        //Activacion de Login on click
+
+        binding.btnLogin.setOnClickListener {
+
+                val email = binding.etEmail.text.toString()
+                val password = binding.etPassword.text.toString()
+
+                if (email.isEmpty()) {
+                    binding.etEmail.error = "Ingresa un e-mail"
+                    return@setOnClickListener
                 }
-            } catch (e: Exception){
-                Log.e("Error enviarNombre", e.message.toString())
-            }
 
-        }
-        binding.btnRegister.setOnClickListener{
-            try {
-                val nombre: String = binding.Username.text.toString()
-                val clave: String = binding.Clave.text.toString()
-                val repetirclave : String = binding.RepetirClave.text.toString()
-
-                if (nombre.isNotEmpty() && clave.isNotEmpty() && repetirclave.isNotEmpty()){
-                    val intent= Intent(this, VistaBienvenida::class.java)
-                    intent.putExtra("nombre",nombre)
-                    startActivity(intent)
-                }else{
-                    Toast.makeText(this, "Faltan campos por llenar",Toast.LENGTH_LONG).show()
+                if (password.isEmpty()) {
+                    binding.etPassword.error = "Ingresa una password"
                 }
-            }catch (e: Exception){
-                Toast.makeText(this, "Error: ${e.message}",Toast.LENGTH_SHORT).show()
-            }
         }
-
     }
 }
